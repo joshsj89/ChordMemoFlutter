aaf609e8ce35c70a0c4faf2de09776fa5b86e4bd
joshsj89 <joshsj89@gmail.com>
2024-12-18 07:59:46 -0800

    Add song list functionality on Home Screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,109 @@
 import 'package:flutter/material.dart';
 
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
 import 'about_screen.dart';
 import 'export_import_screen.dart';
+
+class Key {
+  final String tonic;
+  final String symbol;
+  final String mode;
+
+  Key({
+    required this.tonic,
+    required this.symbol,
+    required this.mode,
+  });
+
+  factory Key.fromJson(Map<String, dynamic> json) {
+    return Key(
+      tonic: json['tonic'],
+      symbol: json['symbol'],
+      mode: json['mode'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'tonic': tonic,
+      'symbol': symbol,
+      'mode': mode,
+    };
+  }
+}
+
+class Section {
+  final String sectionTitle;
+  final Key key;
+  final String chords;
+
+  Section({
+    required this.sectionTitle,
+    required this.key,
+    required this.chords,
+  });
+
+  factory Section.fromJson(Map<String, dynamic> json) {
+    return Section(
+      sectionTitle: json['sectionTitle'],
+      key: Key(
+        tonic: json['key']['tonic'],
+        symbol: json['key']['symbol'],
+        mode: json['key']['mode'],
+      ),
+      chords: json['chords'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'sectionTitle': sectionTitle,
+      'key': key.toJson(),
+      'chords': chords,
+    };
+  }
+}
+
+class Song {
+  final String id;
+  final String title;
+  final String artist;
+  final List<String> genres;
+  final List<Section> sections;
+
+
+  Song({
+    required this.id,
+    required this.title,
+    required this.artist,
+    required this.genres,
+    required this.sections,
+  });
+
+  factory Song.fromJson(Map<String, dynamic> json) {
+    return Song(
+      id: json['id'],
+      title: json['title'],
+      artist: json['artist'],
+      genres: List<String>.from(json['genres']),
+      sections: (json['sections'] as List)
+        .map((section) => Section.fromJson(section))
+        .toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'artist': artist,
+      'genres': genres,
+      'sections': sections.map((section) => section.toJson()).toList(),
+    };
+  }
+}
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,6 +114,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   bool isDarkMode = false;
+  List<Song> songs = [];
 
   void _toggleDarkMode() {
     setState(() {
@@ -22,7 +125,54 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    // _loadSongs();
+    _loadSongs();
+  }
+
+  Future<void> _loadSongs() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedSongs = prefs.getString('songs');
+
+    if (savedSongs != null) {
+      setState(() {
+        songs = (jsonDecode(savedSongs) as List)
+          .map((song) => Song.fromJson(song))
+          .toList();
+      });
+    }
+  }
+
+  Future<void> _deleteSong(String songId) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      songs.removeWhere((song) => song.id == songId);
+    });
+
+    await prefs.setString('songs', jsonEncode(songs));
+  }
+
+  void _confirmDelete(Song song) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: const Text('Confrim Deletion'),
+        content: Text('Are you sure you want to delete ${song.title}?'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(ctx).pop(),
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(ctx).pop();
+              _deleteSong(song.id);
+            },
+            style: TextButton.styleFrom(foregroundColor: Colors.red),
+            child: const Text('Delete'),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -131,13 +281,51 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         )
       ),
-      body: Center(),
+      body: ListView.builder(
+        itemCount: songs.length,
+        itemBuilder: (context, index) {
+          final song = songs[index];
+
+          return InkWell(
+            onTap: () {
+              // TODO: Navigate to SongDetails screen
+              // Example: Navigator.push(context, MaterialPageRoute(builder: (_) => SongDetailsScreen(song: song)));
+            },
+            onLongPress: () => _confirmDelete(song),
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.grey)),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    song.title,
+                    style: TextStyle(
+                      color: textColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    song.artist,
+                    style: TextStyle(
+                      color: isDarkMode ? Color(0xff99999e) : Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: _addArbitrarySong,
         tooltip: 'Add Song',
         backgroundColor: Color(0xff009788),
         child: Icon(Icons.add, color: backgroundColor),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
