7488ceea90ee0fc5dccabf35e1001865e8232f5a
joshsj89 <joshsj89@gmail.com>
2024-12-21 08:42:01 -0800

    Made Autocomplete Dropdown show on top of other widgets

--- lib/view/autocomplete_dropdown.dart
+++ lib/view/autocomplete_dropdown.dart
@@ -29,6 +29,9 @@ class AutocompleteDropdown extends StatefulWidget {
 class _AutocompleteDropdownState extends State<AutocompleteDropdown> {
   final TextEditingController _controller = TextEditingController(); // Controller for the text field
   final FocusNode _focusNode = FocusNode(); // for handling focus events
+  final LayerLink _layerLink = LayerLink(); // for positioning the suggestion list
+  OverlayEntry? _overlayEntry; // The overlay entry for the suggestion list
+
   List<String> _filteredSuggestions = []; // Suggestions that match the current input
   bool _showDropdown = false;
 
@@ -48,6 +51,7 @@ class _AutocompleteDropdownState extends State<AutocompleteDropdown> {
       .toList();
 
       _showDropdown = input.isNotEmpty && _filteredSuggestions.isNotEmpty;
+      _updateOverlay();
     });
 
     widget.onChanged(input);
@@ -57,96 +61,135 @@ class _AutocompleteDropdownState extends State<AutocompleteDropdown> {
     if (_focusNode.hasFocus) {
       _onTextChanged();
     } else {
-      setState(() {
-        _showDropdown = false;
-      });
+      _hideOverlay();
     }
+  }
+
+  void _updateOverlay() {
+    if (_showDropdown) {
+      _overlayEntry?.remove();
+      _overlayEntry = _createOverlayEntry();
+      Overlay.of(context).insert(_overlayEntry!);
+    } else {
+      _hideOverlay();
+    }
+  }
+
+  void _hideOverlay() {
+    _overlayEntry?.remove();
+    _overlayEntry = null;
+
+    setState(() {
+      _showDropdown = false;
+    });
+  }
+
+  OverlayEntry _createOverlayEntry() {
+    final RenderBox renderBox = context.findRenderObject() as RenderBox; // Get the render box of the text field
+    final Size size = renderBox.size; // Get the size of the text field
+    final Offset offset = renderBox.localToGlobal(Offset.zero); // Get the global position of the text field
+
+    return OverlayEntry(
+      builder: (context) {
+        return Positioned(
+          left: offset.dx, // Set the left position to the x-coordinate of the text field
+          top: offset.dy + size.height, // Set the top position to the y-coordinate of the text field + its height
+          width: size.width, // Set the width to the width of the text field
+          child: CompositedTransformFollower(
+            link: _layerLink,
+            showWhenUnlinked: false,
+            offset: Offset(0, size.height),
+            child: Material(
+              elevation: 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: widget.borderSide.color),
+                  borderRadius: BorderRadius.circular(4),
+                  color: widget.suggestionListBackgroundColor,
+                ),
+                constraints: BoxConstraints(
+                  maxHeight: (widget.maxVisibleSuggestions * 56.0).toDouble(), // 56.0 is the height of a ListTile
+                ),
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: _filteredSuggestions.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(
+                        _filteredSuggestions[index],
+                        style: widget.style,
+                      ),
+                      onTap: () {
+                        setState(() {
+                          _controller.text = _filteredSuggestions[index];
+                          _hideOverlay();
+
+                          widget.onChanged(_controller.text);
+                        });
+
+                        _focusNode.unfocus();
+                      },
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        TextField(
-          controller: _controller,
-          focusNode: _focusNode,
-          style: widget.style,
-          decoration: InputDecoration(
-            hintText: widget.hintText,
-            hintStyle: widget.hintStyle,
-            suffixIcon: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                if (_controller.text.isNotEmpty)
-                  IconButton(
-                    icon: const Icon(Icons.cancel),
-                    onPressed: () {
-                      setState(() {
-                        _controller.clear();
-                        _showDropdown = false;
-                      });
-                    },
-                  ),
-                AnimatedRotation(
-                  turns: _showDropdown ? 0.5 : 0, // 0.5*2*π = 1π = 180°
-                  duration: const Duration(milliseconds: 300),
-                  child: IconButton(
-                    icon: const Icon(Icons.expand_more),
-                    onPressed: () {
-                      setState(() {
-                        _showDropdown = !_showDropdown;
-                      });
-                    },
-                  ),
+    return CompositedTransformTarget(
+      link: _layerLink,
+      child: TextField(
+        controller: _controller,
+        focusNode: _focusNode,
+        style: widget.style,
+        decoration: InputDecoration(
+          hintText: widget.hintText,
+          hintStyle: widget.hintStyle,
+          suffixIcon: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (_controller.text.isNotEmpty)
+                IconButton(
+                  icon: const Icon(Icons.cancel),
+                  onPressed: () {
+                    _controller.clear();
+                    _hideOverlay();
+                  },
                 ),
-              ],
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: widget.borderSide,
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: widget.borderSide,
-            ),
-          ),
-        ),
-
-        if (_showDropdown)
-          Container(
-            margin: const EdgeInsets.only(top: 4),
-            decoration: BoxDecoration(
-              border: Border.all(color: widget.borderSide.color),
-              borderRadius: BorderRadius.circular(4),
-              color: widget.suggestionListBackgroundColor,
-            ),
-            constraints: BoxConstraints(
-              maxHeight: (widget.maxVisibleSuggestions * 56.0).toDouble(), // 56.0 is the height of a ListTile
-            ),
-            child: ListView.builder(
-              shrinkWrap: true,
-              // physics: const NeverScrollableScrollPhysics(), // Disable scrolling
-              itemCount: _filteredSuggestions.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(
-                    _filteredSuggestions[index],
-                    style: widget.style,
-                  ),
-                  onTap: () {
+              AnimatedRotation(
+                turns: _showDropdown ? 0.5 : 0, // 0.5*2*π = 1π = 180°
+                duration: const Duration(milliseconds: 300),
+                child: IconButton(
+                  icon: const Icon(Icons.expand_more),
+                  onPressed: () {
                     setState(() {
-                      _controller.text = _filteredSuggestions[index];
-                      _showDropdown = false;
-
-                      widget.onChanged(_controller.text);
+                      _showDropdown = !_showDropdown;
                     });
 
-                    _focusNode.unfocus();
+                    if (_showDropdown) {
+                      _updateOverlay();
+                    } else {
+                      _hideOverlay();
+                    }
                   },
-                );
-              },
-            ),
+                ),
+              ),
+            ],
           ),
-      ],
+          enabledBorder: OutlineInputBorder(
+            borderSide: widget.borderSide,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: widget.borderSide,
+          ),
+        ),
+      ),
     );
   }
 
@@ -154,6 +197,7 @@ class _AutocompleteDropdownState extends State<AutocompleteDropdown> {
   void dispose() { // Clean up the controller and focus node when the widget is disposed
     _controller.dispose();
     _focusNode.dispose();
+    _overlayEntry?.remove();
     super.dispose();
   }
 }
