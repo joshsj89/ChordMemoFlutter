996ee662010268aed9ea446917388ed694c26798
joshsj89 <joshsj89@gmail.com>
2025-01-04 02:46:09 -0800

    Add transformChords function

--- lib/view_model/chords.dart
+++ lib/view_model/chords.dart
@@ -7,13 +7,32 @@ List<String> splitChordsIntoArray(String chords) {
   final chordArray = chords.split(RegExp(r'(\s|-|\(|\))')).where((part) => part.isNotEmpty).toList();
   final List<String> updatedChordArray = [];
 
-  chordArray.forEach((part){
+  for(var part in chordArray) {
     if (part.trim().isNotEmpty && part != '-') {
       updatedChordArray.add(part);
     } else if (part == ' ') {
       updatedChordArray.add(part);
     }
-  });
+  }
 
   return updatedChordArray;
+}
+
+String transformChords(List<String> chords) {
+  return chords.join('-').replaceAllMapped(
+    RegExp(r'-\s-|- |-\(|-\)'),
+    (match) {
+      switch (match[0]) {
+        case '- -':
+        case '- ':
+          return ' '; // Replace dash with a space
+        case '(-':
+          return '('; // Remove dash after '('
+        case '-)':
+          return ')'; // Remove dash before ')'
+        default:
+          return ''; // Default fallback
+      }
+    },
+  );
 }
