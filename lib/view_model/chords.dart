feec8ea816543885c147d89c48af4587756a8f3e
joshsj89 <joshsj89@gmail.com>
2025-03-27 21:41:59 -0700

    Fix bug with incorrect parsing of chords string

--- lib/view_model/chords.dart
+++ lib/view_model/chords.dart
@@ -4,7 +4,7 @@ List<String> splitChordsIntoArray(String chords) {
   }
 
   // split chord string into parts
-  final chordArray = chords.split(RegExp(r'(\s|-|\(|\))')).where((part) => part.isNotEmpty).toList();
+  final chordArray = chords.splitWithDelim(RegExp(r'(\s|-|\(|\))')).where((part) => part.isNotEmpty).toList();
   final List<String> updatedChordArray = [];
 
   for(var part in chordArray) {
@@ -35,4 +35,27 @@ String transformChords(List<String> chords) {
       }
     },
   );
+}
+
+// Extensions allow you to add methods to existing classes
+// without modifying the class or creating a new class that inherits from it.
+
+// Adding allMatchesWithSep method to RegExp class
+extension RegExpExtension on RegExp {
+  List<String> allMatchesWithSep(String input, [int start = 0]) {
+    var result = <String>[];
+    for (var match in allMatches(input, start)) {
+      result.add(input.substring(start, match.start));
+      result.add(match[0]!);
+      start = match.end;
+    }
+    result.add(input.substring(start));
+    return result;
+  }
+}
+
+// Adding splitWithDelim method to String class
+extension StringExtension on String {
+  List<String> splitWithDelim(RegExp pattern) =>
+      pattern.allMatchesWithSep(this);
 }
